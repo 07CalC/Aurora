75b5e61fabffae9394ad7b850e55210afa0d3107
calc <maheshwarivinayak90@gmail.com>
2025-06-18 12:12:20 +0530

    symlink fifx

---
+++ flex/flex.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+kitty --class nivm -e nvim &
+sleep 0.3
+kitty --class kitty-dashboard &
+sleep 0.3
+kitty --class cava -e cava &
+sleep 0.3
+kitty --class peaclock -e peaclock
+sleep 0.3
+
+
+
+
